e6927b1b63f60c5b1407c7ad4c625e48a5df241e
rmwenzel <rmwenzel@uw.edu>
2022-02-06 19:30:04 -0800

    Copy from main to full

--- slurm/batch-clean-and-preprocess.sh
+++ slurm/batch-clean-and-preprocess.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=5g
 #SBATCH --job-name='parallel-clean-and-preprocess'
-#SBATCH --output='/home/ena26/covrace/slurm-files/job-outputs/parallel-clean-and-preprocess-job%j-out.txt'
+#SBATCH --output='/home/ena26/covrace/slurm/job-outputs/parallel-clean-and-preprocess-job%j-out.txt'
 #SBATCH --open-mode=append
 #SBATCH --mail-user=rmw@sidekicktutoring.biz
 #SBATCH --mail-type=ALL
